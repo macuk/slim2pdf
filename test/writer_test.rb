b206d1d66e869bde6179e31722741003038408cf
Piotr Macuk <piotr@macuk.pl>
2014-01-14 10:58:32 +0100

    Small test refactoring

--- test/writer_test.rb
+++ test/writer_test.rb
@@ -2,24 +2,26 @@ require 'test_helper'
 
 module Slim2pdf
   class WriterTest < MiniTest::Unit::TestCase
+    def setup
+      @data = {a: 10, b: 20}
+    end
+
     def test_init
       Writer.new
     end
 
     def test_initialize
-      h = {a: 10, b: 20}
-      writer = Writer.new('/tmp/tpl.slim', h)
+      writer = Writer.new('/tmp/tpl.slim', @data)
       assert_equal '/tmp/tpl.slim', writer.template
-      assert_equal h, writer.data
+      assert_equal @data, writer.data
     end
 
     def test_accessors
-      h = {a: 10, b: 20}
       writer = Writer.new
       writer.template = '/tmp/tpl.slim'
-      writer.data = h
+      writer.data = @data
       assert_equal '/tmp/tpl.slim', writer.template
-      assert_equal h, writer.data
+      assert_equal @data, writer.data
     end
   end
 end
