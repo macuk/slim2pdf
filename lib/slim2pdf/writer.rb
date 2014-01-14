dca97d319b1b641a08f1df49bc7529a78d56c430
Piotr Macuk <piotr@macuk.pl>
2014-01-14 10:50:22 +0100

    Writer accessors

--- lib/slim2pdf/writer.rb
+++ lib/slim2pdf/writer.rb
@@ -1,4 +1,10 @@
 module Slim2pdf
   class Writer
+    attr_accessor :template, :data
+
+    def initialize(template=nil, data={})
+      @template = template
+      @data = data
+    end
   end
 end
--- test/writer_test.rb
+++ test/writer_test.rb
@@ -5,5 +5,21 @@ module Slim2pdf
     def test_init
       Writer.new
     end
+
+    def test_initialize
+      h = {a: 10, b: 20}
+      writer = Writer.new('/tmp/tpl.slim', h)
+      assert_equal '/tmp/tpl.slim', writer.template
+      assert_equal h, writer.data
+    end
+
+    def test_accessors
+      h = {a: 10, b: 20}
+      writer = Writer.new
+      writer.template = '/tmp/tpl.slim'
+      writer.data = h
+      assert_equal '/tmp/tpl.slim', writer.template
+      assert_equal h, writer.data
+    end
   end
 end
