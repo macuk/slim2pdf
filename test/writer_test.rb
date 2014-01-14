c6ead313cb718ef5f1ab2ad58340aed6de1a8c37
Piotr Macuk <piotr@macuk.pl>
2014-01-14 12:16:11 +0100

    Small test refactoring

--- test/writer_test.rb
+++ test/writer_test.rb
@@ -3,7 +3,9 @@ require 'test_helper'
 module Slim2pdf
   class WriterTest < MiniTest::Unit::TestCase
     def setup
-      @data = {a: 10, b: 20}
+      @data = {title: 'Slim2pdf', content: 'Slim to PDF conversion gem'}
+      @template = File.expand_path('../tpl/test.slim', __FILE__)
+      @writer = Writer.new(@template, @data)
     end
 
     def test_init
@@ -11,34 +13,27 @@ module Slim2pdf
     end
 
     def test_initialize
-      writer = Writer.new('/tmp/tpl.slim', @data)
-      assert_equal '/tmp/tpl.slim', writer.template
-      assert_equal @data, writer.data
+      assert_equal @template, @writer.template
+      assert_equal @data, @writer.data
     end
 
     def test_accessors
       writer = Writer.new
-      writer.template = '/tmp/tpl.slim'
+      writer.template = @template
       writer.data = @data
-      assert_equal '/tmp/tpl.slim', writer.template
+      assert_equal @template, writer.template
       assert_equal @data, writer.data
     end
 
     def test_render_to_html
-      data = {title: 'Slim2pdf', content: 'Slim to PDF conversion gem'}
-      template = File.expand_path('../tpl/test.slim', __FILE__)
-      writer = Writer.new(template, data)
-      html = writer.render_to_html
+      html = @writer.render_to_html
       assert_match %r(<title>Slim2pdf</title>), html
       assert_match %r(<p>Slim to PDF conversion gem</p>), html
     end
 
     def test_save_to_html
-      data = {title: 'Slim2pdf', content: 'Slim to PDF conversion gem'}
-      template = File.expand_path('../tpl/test.slim', __FILE__)
-      writer = Writer.new(template, data)
       path = '/tmp/test.html'
-      writer.save_to_html(path)
+      @writer.save_to_html(path)
       assert File.exists?(path)
       html = File.read(path)
       assert_match %r(<title>Slim2pdf</title>), html
@@ -48,10 +43,8 @@ module Slim2pdf
     end
 
     def test_save_to_html_with_dir_creation
-      template = File.expand_path('../tpl/test.slim', __FILE__)
-      writer = Writer.new(template)
       path = '/tmp/a/b/c/test.html'
-      writer.save_to_html(path)
+      @writer.save_to_html(path)
       html = File.read(path)
       assert_match %r(<html>), html
       FileUtils.rm_rf('/tmp/a')
